4db64ecc1eae920c9aceaeb320957a6d82fdf647
Martin Bedouret <martinbedouret@gmail.com>
2024-09-27 17:31:07 -0300

    wip

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -12,6 +12,7 @@ import particleSpray from './particle-spray';
 import useStyles from './banner-style';
 import { motion } from 'framer-motion';
 import { Button } from '@mui/material';
+import { sendGAEvent } from '@next/third-parties/google'
 
 function Banner() {
   const { classes, cx } = useStyles();
@@ -59,6 +60,7 @@ function Banner() {
                 </Typography>
                 <Button
                   component={Link} href="https://app.cboard.io/"
+                  onClick={() => sendGAEvent('event', 'open_web_app', 'cta')}
                   aria-label="cboard-app"
                   variant="contained"
                   className={classes.openCboard}
@@ -66,16 +68,24 @@ function Banner() {
                   {t('mobile-landing.cta_btn')}
                 </Button>
                 <div className={classes.btnArea}>
-                  <Link href="https://apps.apple.com/gb/app/aac-cboard-app/id6453683048">
+                  <Link
+                    href="https://apps.apple.com/gb/app/aac-cboard-app/id6453683048"
+                    onClick={() => sendGAEvent('event', 'open_ios_store', 'cta')}>
                     <img src="/images/mobile/app-store.png" alt="app store" />
                   </Link>
-                  <Link href="https://play.google.com/store/apps/details?id=com.unicef.cboard">
+                  <Link
+                    href="https://play.google.com/store/apps/details?id=com.unicef.cboard"
+                    onClick={() => sendGAEvent('event', 'open_android_store', 'cta')}>
                     <img src="/images/mobile/play-store.png" alt="play store" />
                   </Link>
-                  <Link href="https://apps.microsoft.com/store/detail/XP9M5KQV699FLR">
+                  <Link
+                    href="https://apps.microsoft.com/store/detail/XP9M5KQV699FLR"
+                    onClick={() => sendGAEvent('event', 'open_microsoft_store', 'cta')}>
                     <img src="/images/mobile/ms-store.png" alt="microsoft store" />
                   </Link>
-                  <Link href="https://www.amazon.com/-/en/dp/B0BWGZBQ7V/ref=sr_1_2?crid=3KHHTU961DJKE&keywords=cboard&qid=1684203554&s=mobile-apps&sprefix=%2Cmobile-apps%2C245&sr=1-2">
+                  <Link
+                    href="https://www.amazon.com/-/en/dp/B0BWGZBQ7V/ref=sr_1_2?crid=3KHHTU961DJKE&keywords=cboard&qid=1684203554&s=mobile-apps&sprefix=%2Cmobile-apps%2C245&sr=1-2"
+                    onClick={() => sendGAEvent('event', 'open_amazon_store', 'cta')}>
                     <img src="/images/mobile/amazon-store.png" alt="amazon store" />
                   </Link>
                 </div>
