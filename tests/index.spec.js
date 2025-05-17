36cce0e1403f441c28c3245c8292c70a6a4c6001
martin bedouret <martinbedouret@gmail.com>
2025-05-17 03:02:26 -0300

    Adds Playwright for E2E testing
    Introduces Playwright for end-to-end testing.
    
    Sets up the configuration file and adds example tests to ensure
    the basic functionality of the landing page and a demo todo app.
    Also adds playwright related files to .gitignore.

---
+++ tests/index.spec.js
@@ -0,0 +1,19 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.cboard.io/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Cboard AAC | Home - Communication Boards for All/);
+});
+
+test('open cboard link', async ({ page }) => {
+  await page.goto('https://www.cboard.io/');
+
+  // Click the get started link.
+  await page.getByLabel('cboard-app').click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.locator('.WelcomeScreen__container')).toBeVisible();
+});
