d08f6545631be46383572166a3f2c49a01b40a3e
martin bedouret <martinbedouret@gmail.com>
2025-08-19 23:31:53 -0300

    Add voice cloning form page

---
+++ pages/[locale]/voice-cloning.js
@@ -0,0 +1,69 @@
+import React, { Fragment } from 'react';
+import Head from 'next/head';
+import { makeStyles } from 'tss-react/mui';
+import { getStaticPaths, makeStaticProps } from 'lib/getStatic';
+import brand from 'public/text/brand';
+import VoiceCloningForm from 'components/Forms/VoiceCloning';
+import { CssBaseline, useMediaQuery } from '@mui/material';
+import MainContainer from 'components/MainContainer';
+import PropTypes from 'prop-types';
+import PageNav from 'components/PageNav';
+
+const useStyles = makeStyles({ uniqId: 'voice-cloning' })(theme => ({
+  mainWrap: {
+    background: theme.palette.background.paper,
+  },
+  containerWrap: {
+    marginTop: 40,
+    marginBottom: theme.spacing(40),
+    '& > section': {
+      position: 'relative'
+    }
+  }
+}));
+
+function VoiceCloning(props) {
+  const { classes } = useStyles();
+  const { onToggleDark, onToggleDir } = props;
+  const isTablet = useMediaQuery(theme => theme.breakpoints.down('lg'));
+
+  return (
+    <Fragment>
+      <Head>
+        <title>
+          {brand.cboard.name + ' - Voice Cloning Application'}
+        </title>
+      </Head>
+      <CssBaseline />
+      <MainContainer
+        onToggleDark={onToggleDark}
+        onToggleDir={onToggleDir}
+        footerDeco
+      >
+        <Fragment>
+          <main className={classes.containerWrap}>
+            <section id="voice-cloning">
+              <VoiceCloningForm />
+            </section>
+          </main>
+          {!isTablet && (
+            <Fragment>
+              <PageNav />
+            </Fragment>
+          )}
+        </Fragment>
+      </MainContainer>
+    </Fragment>
+  );
+}
+
+VoiceCloning.propTypes = {
+  onToggleDark: PropTypes.func.isRequired,
+  onToggleDir: PropTypes.func.isRequired,
+};
+
+// Use this below for Static Site Generation (SSG)
+const getStaticProps = makeStaticProps(['common']);
+export { getStaticPaths, getStaticProps };
+
+export default VoiceCloning;
